3340a3739b413c06231981fb97e14b90fe391c49
Vadim Belousov <thevadiratti@yandex.ru>
2023-02-24 10:52:49 +0500

    feat: добавить функцию randomArr

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,6 +10,18 @@ export const swap = (array: any[], firstIndex: number, secondIndex: number) => {
 
 const setDelay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
+export const randomArr = (): number[] => {
+  const length = Math.floor(Math.random() * (17 - 3) + 3);
+  const arr = [];
+
+  for(let i = 0; i < length; i++) {
+    const newNumb = Math.random() * (100 + 1);
+
+    arr.push(Math.floor(newNumb));
+  }
+  return arr;
+}
+
 export const recursionArray = async (array: TStringObj[], setArray: React.Dispatch<React.SetStateAction<TStringObj[]>>, setProgress: (value: React.SetStateAction<boolean>) => void) => {
   let start = 0;
   let end = array.length - 1;
