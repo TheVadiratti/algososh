8832737c59683369b3b319cf061c50d46875948e
Vadim Belousov <thevadiratti@yandex.ru>
2023-02-27 11:19:17 +0500

    feat: добавить класс Queue

---
+++ src/pages/queue-page/Queue.ts
@@ -0,0 +1,51 @@
+interface IQueue<T> {
+  enqueue: (item: T) => void;
+  dequeue: () => void;
+  reset: () => void;
+  isEmpty: () => boolean;
+}
+
+export class Queue<T> implements IQueue<T> {
+  private container: (T | null)[] = [];
+  private head: number = 0;
+  private tail: number = 0;
+  private readonly size: number = 0;
+  private length: number = 0;
+
+  constructor(size: number) {
+    this.size = size;
+    this.container = Array(size);
+  }
+
+  enqueue = (item: T): void => {
+    if (this.container.length <= this.size) {
+      this.container[this.tail] = item;
+      this.tail++;
+      this.length++;
+    }
+    else {
+      console.log('Maximum length exceeded');
+    }
+  }
+
+  dequeue = (): void => {
+    if (this.container.length) {
+      this.container[this.head] = null;
+      this.head++;
+      this.length--;
+    }
+    else {
+      console.log('Queue is empty');
+    }
+  }
+
+  reset = (): void => {
+    this.container = Array(this.size);
+    this.head = 0;
+    this.tail = 0;
+  };
+
+  isEmpty = (): boolean => {
+    return this.length === 0;
+  }
+}
