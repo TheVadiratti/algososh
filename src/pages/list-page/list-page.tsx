de1543da9d9c473f911800cddbea20c1f68f720a
Vadim Belousov <thevadiratti@yandex.ru>
2023-03-01 13:06:17 +0500

    feat: реализовать отрисовку начального списка

--- src/pages/list-page/list-page.tsx
+++ src/pages/list-page/list-page.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import Styles from './list-page.module.css';
+import Styles from "./list-page.module.css";
 import { Button } from "../../components/ui/button/button";
 import { Input } from "../../components/ui/input/input";
 import { SolutionLayout } from "../../components/ui/solution-layout/solution-layout";
+import { List } from "./List";
+import { TElement } from "../../types/types";
+import { ElementStates } from "../../types/element-states";
+import { Circle } from "../../components/ui/circle/circle";
+
+const list = new List<string>();
+list.append('0');
+list.append('34');
+list.append('8');
+list.append('1');
 
 export const ListPage: React.FC = () => {
+  const [state, setState] = React.useState<TElement<string>[]>([]);
+
+  const convertQueue = (list: List<string>) => {
+    return list.getElements().map(item => {
+      return {
+        value: item,
+        state: ElementStates.Default
+      }
+    })
+  }
+
+  React.useEffect(() => {
+    const initialState = convertQueue(list);
+    setState([...initialState]);
+  }, [])
+
   return (
     <SolutionLayout title="Связный список">
       <form className={Styles.form}>
@@ -54,7 +80,18 @@ export const ListPage: React.FC = () => {
         </fieldset>
       </form>
       <div className={Styles.result}>
-
+        {state.map((item, i) => {
+          return (
+            <Circle
+              letter={item.value}
+              state={item.state}
+              head={i === 0 ? 'head' : ''}
+              tail={i === state.length - 1 ? 'tail' : ''}
+              index={i}
+              key={i}
+            />
+          )
+        })}
       </div>
     </SolutionLayout>
   );
